c81198bbc07165cb594c929c93d7fc751571f637
Alghifari Muhammad Dzar <alghifari.m.d@gmail.com>
2020-01-04 13:12:20 +0300

    Tugas 26 JS
    Tugas ini untuk Niomic

---
+++ Tugas26.js
@@ -0,0 +1,15 @@
+function tugas26() {
+  var numbers = [2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16];
+
+  for(i=0; i<numbers.length; i++){
+
+    if (isFinite(numbers[i]===false)) {
+      console.log("Angka", numbers[i], "NOT Infinity")
+    }
+    else {
+      console.log("Angka", numbers[i], "Infinity")
+    }
+  }
+}
+
+tugas26();
